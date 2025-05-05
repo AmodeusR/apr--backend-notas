bfdc410fd55d958192dd1e42e616cff89ef452a7
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-05-04 23:48:10 -0300

    refactor: change parseZodError function name
    change util function name to better describe its role

--- src/utils/parseZodError.ts
+++
@@ -1,9 +0,0 @@
-import type { z } from "zod";
-
-export function parseZodError(issue: z.core.$ZodIssue[]) {
-  const error =  issue[0];
-  const description = error.message;
-  const field = String(error.path[0]);
-
-  return { description, field };
-}
