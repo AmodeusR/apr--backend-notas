3c0a5569581a39805a2d77512a01e2fea68da32d
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-05-13 11:58:12 -0300

    refactor: update seed to create user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,39 +1,48 @@
-import { PrismaClient } from "../src/generated/prisma";
-const db = new PrismaClient();
+import { db } from "@lib/prisma";
 
 async function main() {
-  console.log("Creating notes...");
-
-  await db.note.deleteMany();
-  await db.note.createMany({
-    data: [
-      {
-        description: "Estudar Vue 3",
-        due: null,
-        expired: false,
-      },
-      {
-        description: "Finalizar o backend com Fastify",
-        due: null,
-        expired: false,
-      },
-      {
-        description: "Refatorar componente de tarefas",
-        due: null,
-        expired: false,
-      },
-      {
-        description: "Ler documentação do Prisma",
-        due: null,
-        expired: false,
-      },
-      {
-        description: "Criar testes para a API",
-        due: null,
-        expired: false,
-      },
-    ],
+  
+  console.log("Creating user...");
+  await db.user.deleteMany();
+  const user = await db.user.create({
+    data: {
+      username: "admin",
+      email: "admin@localhost",
+      password: await Bun.password.hash("123456")
+    }
   });
+  console.log("User created!");
+  
+  console.log("Creating notes...");
+  const notes = [
+    {
+      description: "Estudar Vue 3",
+      due: null,
+    },
+    {
+      description: "Finalizar o backend com Fastify",
+      due: null,
+    },
+    {
+      description: "Refatorar componente de tarefas",
+      due: null,
+    },
+    {
+      description: "Ler documentação do Prisma",
+      due: null,
+    },
+    {
+      description: "Criar testes para a API",
+      due: null,
+    },
+  ];
+  
+  await db.note.deleteMany();
+  notes.forEach(async (note) => {
+    await db.note.create({
+      data: {...note, userId: user.id}
+    });
+  })
 
   console.log("Notes created!");
 }
