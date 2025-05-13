3c869c7bb38fbad63432c46d28a4fc1788be13c4
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-05-13 11:57:55 -0300

    feat: add finished to notes and create user

---
+++ prisma/migrations/20250513123555_create_user_model/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Note` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Note" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "description" TEXT NOT NULL,
+    "due" DATETIME,
+    "finished" BOOLEAN NOT NULL DEFAULT false,
+    "expired" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Note" ("createdAt", "description", "due", "expired", "id", "updatedAt") SELECT "createdAt", "description", "due", "expired", "id", "updatedAt" FROM "Note";
+DROP TABLE "Note";
+ALTER TABLE "new_Note" RENAME TO "Note";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
