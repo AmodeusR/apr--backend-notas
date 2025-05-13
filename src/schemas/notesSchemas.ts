db8e8e661659dfbf47c9f7b5801ad042445e13b0
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-05-13 11:44:45 -0300

    feat: add finished value to NoteSchema

--- src/schemas/notesSchemas.ts
+++ src/schemas/notesSchemas.ts
@@ -3,5 +3,6 @@ import { z } from "zod";
 export const NoteSchema = z.interface({
   description: z.string(),
   due: z.date().optional(),
-  expired: z.boolean()
+  expired: z.boolean().default(false),
+  finished: z.boolean().default(false)
 })
