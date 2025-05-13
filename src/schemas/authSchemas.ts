08de9be9169c6631122fa00a021f0b50d5f4c96c
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-05-13 11:45:20 -0300

    feat: create userRegistrationSchema

---
+++ src/schemas/authSchemas.ts
@@ -0,0 +1,28 @@
+import z from "zod";
+
+const userMessages = {
+  username: {
+    minChars: "Username needs to have at least 3 characters",
+    maxChars: "Username needs to have a maximum of 10 characters",
+  },
+  password: {
+    minChars: "Username needs to have at least 8 characters",
+    uppercase: "The password must have at least 1 uppercase letter",
+    number: "The password must have at least 1 number",
+    symbol: "The password must have at least 1 symbol",
+  },
+};
+
+export const UserRegistrationSchema = z.interface({
+  username: z
+    .string()
+    .min(3, userMessages.username.minChars)
+    .max(10, userMessages.username.maxChars),
+  email: z.email(),
+  password: z
+    .string()
+    .min(8, userMessages.password.minChars)
+    .refine((val) => /[A-Z]/.test(val), userMessages.password.uppercase)
+    .refine((val) => /\d/.test(val), userMessages.password.number)
+    .refine(val => /[!@#$%^&*(),.?":{}|<>]/.test(val), userMessages.password.symbol)
+});
