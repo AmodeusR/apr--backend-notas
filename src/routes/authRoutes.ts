c4a9cfc754fe680939964f138fe65c83dd4c53d3
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-05-26 18:31:51 -0300

    feat: create auth routes (login/logout)

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,11 +1,48 @@
 import { db } from "@lib/prisma";
-import type { userRegistrationData } from "@typings/auth";
+import type { UserLoginData, userRegistrationData } from "@typings/auth";
 import type { FastifyInstance } from "fastify";
 import { PrismaClientKnownRequestError } from "generated/prisma/internal/prismaNamespace";
-import { UserRegistrationSchema } from "schemas/authSchemas";
+import { UserLoginSchema, UserRegistrationSchema } from "schemas/authSchemas";
 import { parseZodIssues } from "utils/parseZodIssues";
+import jwt from "jsonwebtoken";
 
 export function authRoutes(fastify: FastifyInstance) {
+	fastify.post<{Body: UserLoginData}>("/login", async (req, reply) => {
+		const userData = req.body;
+
+		const { success, data, error } = UserLoginSchema.safeParse(userData);
+
+		if (!success) {
+			const { message } = parseZodIssues(error.issues);
+
+			return reply.status(400).send({ message });
+		}
+
+		try {
+			const user = await db.user.findUnique({ where: {
+				email: data.email
+			}});
+
+			if (!user) return reply.status(404).send({ message: "User not found"});
+
+			const isMatch = await Bun.password.verify(data.password, user.password);
+
+			if (isMatch) {
+				// biome-ignore lint: the environment variable exists
+				const token = jwt.sign({ userId: user.id}, Bun.env.JWT_SECRET!, { expiresIn: "1h" });
+	
+				return reply.status(200).send({ token });
+			}
+
+			return reply.status(401).send({ message: "Wrong password provided"});
+		} catch (error) {
+			console.log(error);
+
+			return reply.status(500).send({ error })
+		}
+
+	});
+
 	fastify.post<{ Body: userRegistrationData }>(
 		"/register",
 		async (req, reply) => {
