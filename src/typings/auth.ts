3df3fa245d09243c1583d2f6635f15247effac98
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-05-26 18:29:11 -0300

    feat: UserLoginData

--- src/typings/auth.ts
+++ src/typings/auth.ts
@@ -1,4 +1,6 @@
 import type { UserRegistrationSchema } from "schemas/authSchemas";
+import type { UserLoginSchema, UserRegistrationSchema } from "schemas/authSchemas";
 import type z from "zod";
 
 export type userRegistrationData = z.infer<typeof UserRegistrationSchema>;
+export type UserLoginData = z.infer<typeof UserLoginSchema>;
