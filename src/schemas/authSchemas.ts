4c85df6acfb90d0090da75faa110897b233adb3f
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-06-13 01:02:44 -0300

    fix: update code to work with zod v4 stable version

--- src/schemas/authSchemas.ts
+++ src/schemas/authSchemas.ts
@@ -1,37 +1,37 @@
-import z from "zod";
+import { z } from "zod/v4";
 
 const minimumPasswordChars = 8;
 const userMessages = {
-	username: {
-		minChars: "Username needs to have at least 3 characters",
-		maxChars: "Username needs to have a maximum of 10 characters",
-	},
-	password: {
-		minChars: `Username needs to have at least ${minimumPasswordChars} characters`,
-		uppercase: "The password must have at least 1 uppercase letter",
-		number: "The password must have at least 1 number",
-		symbol: "The password must have at least 1 symbol",
-	},
+  username: {
+    minChars: "Username needs to have at least 3 characters",
+    maxChars: "Username needs to have a maximum of 10 characters",
+  },
+  password: {
+    minChars: `Username needs to have at least ${minimumPasswordChars} characters`,
+    uppercase: "The password must have at least 1 uppercase letter",
+    number: "The password must have at least 1 number",
+    symbol: "The password must have at least 1 symbol",
+  },
 };
 
-export const UserRegistrationSchema = z.interface({
-	username: z
-		.string()
-		.min(3, userMessages.username.minChars)
-		.max(10, userMessages.username.maxChars),
-	email: z.email(),
-	password: z
-		.string()
-		.min(minimumPasswordChars, userMessages.password.minChars)
-		.refine((val) => /[A-Z]/.test(val), userMessages.password.uppercase)
-		.refine((val) => /\d/.test(val), userMessages.password.number)
-		.refine(
-			(val) => /[!@#$%^&*(),.?":{}|<>]/.test(val),
-			userMessages.password.symbol,
-		),
+export const UserRegistrationSchema = z.object({
+  username: z
+    .string()
+    .min(3, userMessages.username.minChars)
+    .max(10, userMessages.username.maxChars),
+  email: z.email(),
+  password: z
+    .string()
+    .min(minimumPasswordChars, userMessages.password.minChars)
+    .refine((val) => /[A-Z]/.test(val), userMessages.password.uppercase)
+    .refine((val) => /\d/.test(val), userMessages.password.number)
+    .refine(
+      (val) => /[!@#$%^&*(),.?":{}|<>]/.test(val),
+      userMessages.password.symbol
+    ),
 });
 
-export const UserLoginSchema = z.interface({
-	email: z.email(),
-	password: z.string(),
+export const UserLoginSchema = z.object({
+  email: z.email(),
+  password: z.string(),
 });
--- src/schemas/notesSchemas.ts
+++ src/schemas/notesSchemas.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const NoteSchema = z.interface({
+export const NoteSchema = z.object({
 	description: z.string(),
 	due: z.date().optional(),
 	expired: z.boolean().default(false),
