7b29b85bfef32485dfa390342d691a213e4b79e2
Ricardo Magalhães <29683600+AmodeusR@users.noreply.github.com>
2025-06-16 08:11:28 -0300

    feat: configure swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
 import { authRoutes } from "@routes/authRoutes";
 import { notesRoutes } from "@routes/notesRoutes";
 import Fastify from "fastify";
+import cookie, { type FastifyCookieOptions } from "@fastify/cookie";
+import fastifySwagger from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
 
 const fastify = Fastify({
 	logger: true,
 });
 
+// Swagger is not properly working with bun at the moment because of some kind of bug
+// https://github.com/oven-sh/bun/issues/20428
+fastify.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: "API Notes",
+			description: "A note creation API with user auth",
+			version: "1.0.0"
+		}
+	}
+});
+
+fastify.register(fastifySwaggerUi, {
+	routePrefix: "/docs",
+});
+
+fastify.register(fastifyCors, { origin: "*"});
+fastify.register(cookie, {
+	secret: process.env.COOKIE_SECRET
+} as FastifyCookieOptions);
 fastify.register(authRoutes, { prefix: "auth/" });
 fastify.register(notesRoutes);
 
